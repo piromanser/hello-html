f858065998f338594bd3d6b1bf04761a006ad0ae
Tim <piromanseix@gmail.com>
2018-01-07 15:04:08 +0300

    add header menu

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+const menuItems = document.querySelectorAll('.menu-item')
+
+menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', event => {
+        alert('You clicked the menu item, congrats!')
+    })
+})
